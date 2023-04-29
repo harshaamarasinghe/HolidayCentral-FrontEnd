6a2871789aa9251ceed7a0a9b58f56d91f84755d
anushasandaruwani <101203297+anushasandaruwani@users.noreply.github.com>
2023-04-29 19:34:23 +0530

    Flight booking

--- holiday-central/src/App.js
+++ holiday-central/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import TravelAgentMenu from "./Components/TravelAgents/TravelAgentsMenu";
 import TravelAgentBookFlight from "./Components/TravelAgents/TravelAgentBookFlight/TravelAgentBookFlight";
--- holiday-central/src/Components/TravelAgents/TravelAgentBookFlight/TravelAgentBookFlight.js
+++ holiday-central/src/Components/TravelAgents/TravelAgentBookFlight/TravelAgentBookFlight.js
@@ -3,6 +3,8 @@ import React from "react";
 import Plane from "./../../../Assets/plane.png";
 import Departure from "./../../../Assets/departures.png";
 import Landing from "./../../../Assets/landing.png";
+import Ticket from "./../../../Assets/ticket.png";
+import Calendar from "./../../../Assets/calendartime.png";
 
 const TravelAgentBookFlight = () => {
   return (
@@ -64,10 +66,298 @@ const TravelAgentBookFlight = () => {
 
       <div class="container my-5"></div>
 
-      <div class="container my-5">
-        <div class="row justify-content-start">
-          <div class="col-1">
-            <img src={Departure} alt="" width={60} />
+      <div class="display-flex">
+        {/* Departure and arrival */}
+        <div class="container my-5 row">
+          <div class="col-3 justify-content-start">
+            <div class="col-1">
+              <img src={Departure} alt="" width={60} />
+            </div>
+            <div class="col-1 fw-bold h3 mt-2 text-secondary">From</div>
+            <div class="col-2 mt-2">
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle bg-white text-dark "
+                  type="button"
+                  id="from-dropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  - Select -
+                </button>
+                <ul
+                  class="dropdown-menu text-start"
+                  aria-labelledby="from-dropdown"
+                >
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Sri Lanka CMB
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Singapore SG
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      UK Liverpool
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 justify-content-start mt-3">
+            <div class="col-1">
+              <img src={Landing} alt="" width={60} />
+            </div>
+            <div class="col-1 fw-bold h3 mt-2 text-secondary">To</div>
+            <div class="col-2 mt-2">
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle bg-white text-dark "
+                  type="button"
+                  id="from-dropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  - Select -
+                </button>
+                <ul
+                  class="dropdown-menu text-start"
+                  aria-labelledby="from-dropdown"
+                >
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Sri Lanka CMB
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Singapore SG
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      UK Liverpool
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Dates */}
+        <div class="container my-5">
+          <div class="row justify-content-start">
+            <div class="col-1">
+              {/* image */}
+              <img src={Ticket} alt="" width={100} />
+            </div>
+            <div class="col-3">
+              <div class="row">
+                {/*row one class*/}
+                <div class="col-2 mt-3">Class</div>
+                <div class="col-2 mt-2">
+                  <div class="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle bg-white text-dark "
+                      type="button"
+                      id="from-dropdown"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      - Select -
+                    </button>
+                    <ul
+                      class="dropdown-menu text-start"
+                      aria-labelledby="from-dropdown"
+                    >
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Economy Class
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Business Class
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                {/*row two person count*/}
+                <div class="col-2 mt-3">Count</div>
+                <div class="col-2 mt-2">
+                  <div class="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle bg-white text-dark "
+                      type="button"
+                      id="from-dropdown"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      - Select -
+                    </button>
+                    <ul
+                      class="dropdown-menu text-start"
+                      aria-labelledby="from-dropdown"
+                    >
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          1
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          2
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          3
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          4
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          5
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          6
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          7
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          8
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Ticket type and no of passengers */}
+        <div class="container my-5">
+          <div class="row justify-content-start">
+            <div class="col-1 mt-2">
+              {/* image */}
+              <img src={Calendar} alt="" width={80} />
+            </div>
+            <div class="col-3">
+              <div class="row">
+                {/*row one class*/}
+                <div class="col-3 mt-3">Departure</div>
+                <div class="col-2 mt-2">
+                  <div class="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle bg-white text-dark "
+                      type="button"
+                      id="from-dropdown"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      - Select -
+                    </button>
+                    <ul
+                      class="dropdown-menu text-start"
+                      aria-labelledby="from-dropdown"
+                    >
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Economy Class
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Business Class
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                {/*row two person count*/}
+                <div class="col-3 mt-3">Return</div>
+                <div class="col-2 mt-2">
+                  <div class="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle bg-white text-dark "
+                      type="button"
+                      id="from-dropdown"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      - Select -
+                    </button>
+                    <ul
+                      class="dropdown-menu text-start"
+                      aria-labelledby="from-dropdown"
+                    >
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          1
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          2
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          3
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          4
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          5
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          6
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          7
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          8
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
