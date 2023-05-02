34f5c0744275b32cc054888d06bea2b6210fe5e8
anushasandaruwani <101203297+anushasandaruwani@users.noreply.github.com>
2023-05-02 11:56:29 +0530

    Hotel Booking Filter

--- holiday-central/src/App.js
+++ holiday-central/src/App.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import TravelAgentMenu from "./Components/TravelAgents/TravelAgentsMenu";
 import TravelAgentBookFlight from "./Components/TravelAgents/FlightReservation/TravelAgentBookFlight/TravelAgentBookFlight";
-import Test from "./Components/TravelAgents/HotelReservation/BookHotel/BookHotel";
+import Test from "./Components/TravelAgents/HotelReservation/FilterHotel/FilterHotel";
 
 import TravelAgentSelectFlight from "./Components/TravelAgents/FlightReservation/TravelAgentSelectFlight/TravelAgentSelectFlight";
 
---
+++ holiday-central/src/Components/TravelAgents/HotelReservation/FilterHotel/FilterHotel.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const BookHotel = () => {
+  return (
+    <div class="p-5 m-5 border rounded justify-content-center">
+      <div class="fs-2 fw-bold justify-content-center my-3">Hotel Booking</div>
+      <form class="row g-3">
+        {/*Star Rating */}
+        <div class="row justify-content-start mt-3">
+          <div class="col-md-2">
+            <label for="inputState" class="form-label">
+              Star Rating
+            </label>
+            <select id="inputState" class="form-select">
+              <option selected>Choose...</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+
+          {/*Price */}
+
+          <div class="col-md-2">
+            <label class="form-label">Minimum Price</label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput"
+              placeholder="Min"
+            />
+          </div>
+
+          <div class="col-md-2">
+            <label class="form-label">Maximum Price</label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput"
+              placeholder="Max"
+            />
+          </div>
+
+          {/*Facilities*/}
+          <div class="col-md-2">
+            <label for="inputState" class="form-label">
+              Facilities
+            </label>
+            <select id="inputState" class="form-select">
+              <option selected>Choose...</option>
+              <option>Pool</option>
+              <option>Kids Play Area</option>
+              <option>Beach Access</option>
+            </select>
+          </div>
+
+          <div class="col-2 mt-4">
+            <button type="submit" class="btn btn-secondary">
+              Filter
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default BookHotel;
